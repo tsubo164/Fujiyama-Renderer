d731926ab841a62b2066b1ebcbb48bd9c93d3aee
Hiroshi Tsubokawa <hiroshi@fujiyama-renderer.com>
2012-02-01 23:17:43 -0800

    use VEC_NTH in Mesh

--- src/Mesh.c
+++ src/Mesh.c
@@ -7,16 +7,12 @@ See LICENSE and README
 #include "LocalGeometry.h"
 #include "Accelerator.h"
 #include "Triangle.h"
+#include "Vector.h"
 #include "Ray.h"
 #include "Box.h"
 #include <stdlib.h>
 #include <string.h>
 #include <float.h>
-
-static void *allocate_P(struct Mesh *mesh, int nverts);
-static void *allocate_N(struct Mesh *mesh, int nverts);
-static void *allocate_uv(struct Mesh *mesh, int nverts);
-static void *allocate_indices(struct Mesh *mesh, int nfaces);
 
 static int triangle_ray_intersect(const void *prim_set, int prim_id, const struct Ray *ray,
 		struct LocalGeometry *isect, double *t_hit);
@@ -48,14 +44,11 @@ void MshFree(struct Mesh *mesh)
 	if (mesh == NULL)
 		return;
 
-	if (mesh->P != NULL)
-		free(mesh->P);
-	if (mesh->N != NULL)
-		free(mesh->N);
-	if (mesh->Cd != NULL)
-		free(mesh->Cd);
-	if (mesh->uv != NULL)
-		free(mesh->uv);
+	free(mesh->P);
+	free(mesh->N);
+	free(mesh->Cd);
+	free(mesh->uv);
+	free(mesh->indices);
 
 	free(mesh);
 }
@@ -66,7 +59,7 @@ void MshComputeBounds(struct Mesh *mesh)
 	BOX3_SET(mesh->bounds, FLT_MAX, FLT_MAX, FLT_MAX, -FLT_MAX, -FLT_MAX, -FLT_MAX);
 
 	for (i = 0; i < mesh->nverts; i++) {
-		BoxAddPoint(mesh->bounds, &mesh->P[3*i]);
+		BoxAddPoint(mesh->bounds, VEC3_NTH(mesh->P,i));
 	}
 }
 
@@ -75,15 +68,19 @@ void *MshAllocateVertex(struct Mesh *mesh, const char *attr_name, int nverts)
 	void *ret = NULL;
 
 	if (strcmp(attr_name, "P") == 0) {
-		ret = allocate_P(mesh, nverts);
+		mesh->P = VEC3_REALLOC(mesh->P, double, nverts);
+		ret = mesh->P;
 	}
 	else if (strcmp(attr_name, "N") == 0) {
-		ret = allocate_N(mesh, nverts);
+		mesh->N = VEC3_REALLOC(mesh->N, double, nverts);
+		ret = mesh->N;
 	}
 	else if (strcmp(attr_name, "uv") == 0) {
-		ret = allocate_uv(mesh, nverts);
+		mesh->uv = VEC2_REALLOC(mesh->uv, float, nverts);
+		ret = mesh->uv;
 	}
 
+	mesh->nverts = nverts;
 	return ret;
 }
 
@@ -92,9 +89,11 @@ void *MshAllocateFace(struct Mesh *mesh, const char *attr_name, int nfaces)
 	void *ret = NULL;
 
 	if (strcmp(attr_name, "indices") == 0) {
-		ret = allocate_indices(mesh, nfaces);
+		mesh->indices = VEC3_REALLOC(mesh->indices, int, nfaces);
+		ret = mesh->indices;
 	}
 
+	mesh->nfaces = nfaces;
 	return ret;
 }
 
@@ -103,12 +102,12 @@ void MshGetFaceVertex(const struct Mesh *mesh, int face_index,
 {
 	int i0, i1, i2;
 
-	i0 = mesh->indices[3*face_index + 0];
-	i1 = mesh->indices[3*face_index + 1];
-	i2 = mesh->indices[3*face_index + 2];
-	*v0 = &mesh->P[3*i0];
-	*v1 = &mesh->P[3*i1];
-	*v2 = &mesh->P[3*i2];
+	i0 = VEC3_NTH(mesh->indices, face_index)[0];
+	i1 = VEC3_NTH(mesh->indices, face_index)[1];
+	i2 = VEC3_NTH(mesh->indices, face_index)[2];
+	*v0 = VEC3_NTH(mesh->P, i0);
+	*v1 = VEC3_NTH(mesh->P, i1);
+	*v2 = VEC3_NTH(mesh->P, i2);
 }
 
 void MshSetupAccelerator(const struct Mesh *mesh, struct Accelerator *acc)
@@ -119,54 +118,6 @@ void MshSetupAccelerator(const struct Mesh *mesh, struct Accelerator *acc)
 			mesh->bounds,
 			triangle_ray_intersect,
 			triangle_bounds);
-}
-
-static void *allocate_P(struct Mesh *mesh, int nverts)
-{
-	mesh->P = (double *) realloc(mesh->P, 3 * sizeof(double) * nverts);
-	if (mesh->P == NULL) {
-		mesh->nverts = 0;
-		return NULL;
-	}
-
-	mesh->nverts = nverts;
-	return mesh->P;
-}
-
-static void *allocate_N(struct Mesh *mesh, int nverts)
-{
-	mesh->N = (double *) realloc(mesh->N, 3 * sizeof(double) * nverts);
-	if (mesh->N == NULL) {
-		mesh->nverts = 0;
-		return NULL;
-	}
-
-	mesh->nverts = nverts;
-	return mesh->N;
-}
-
-static void *allocate_uv(struct Mesh *mesh, int nverts)
-{
-	mesh->uv = (float *) realloc(mesh->uv, 2 * sizeof(float) * nverts);
-	if (mesh->uv == NULL) {
-		mesh->nverts = 0;
-		return NULL;
-	}
-
-	mesh->nverts = nverts;
-	return mesh->uv;
-}
-
-static void *allocate_indices(struct Mesh *mesh, int nfaces)
-{
-	mesh->indices = (int *) realloc(mesh->indices, 3 * sizeof(int) * nfaces);
-	if (mesh->indices == NULL) {
-		mesh->nfaces = 0;
-		return NULL;
-	}
-
-	mesh->nfaces = nfaces;
-	return mesh->indices;
 }
 
 static int triangle_ray_intersect(const void *prim_set, int prim_id, const struct Ray *ray,
@@ -180,12 +131,12 @@ static int triangle_ray_intersect(const void *prim_set, int prim_id, const struc
 	int hit;
 
 	/* TODO make function */
-	i0 = mesh->indices[3*prim_id + 0];
-	i1 = mesh->indices[3*prim_id + 1];
-	i2 = mesh->indices[3*prim_id + 2];
-	v0 = &mesh->P[3*i0];
-	v1 = &mesh->P[3*i1];
-	v2 = &mesh->P[3*i2];
+	i0 = VEC3_NTH(mesh->indices, prim_id)[0];
+	i1 = VEC3_NTH(mesh->indices, prim_id)[1];
+	i2 = VEC3_NTH(mesh->indices, prim_id)[2];
+	v0 = VEC3_NTH(mesh->P, i0);
+	v1 = VEC3_NTH(mesh->P, i1);
+	v2 = VEC3_NTH(mesh->P, i2);
 
 	hit = TriRayIntersect(
 			v0, v1, v2,
@@ -199,12 +150,9 @@ static int triangle_ray_intersect(const void *prim_set, int prim_id, const struc
 		return 1;
 
 	/* intersect info */
-	/*
-	TriComputeFaceNormal(isect->N, v0, v1, v2);
-	*/
-	N0 = &mesh->N[3*i0];
-	N1 = &mesh->N[3*i1];
-	N2 = &mesh->N[3*i2];
+	N0 = VEC3_NTH(mesh->N, i0);
+	N1 = VEC3_NTH(mesh->N, i1);
+	N2 = VEC3_NTH(mesh->N, i2);
 	TriComputeNormal(isect->N, N0, N1, N2, u, v);
 
 	/* TODO TMP uv handling */
@@ -212,9 +160,9 @@ static int triangle_ray_intersect(const void *prim_set, int prim_id, const struc
 	if (mesh->uv != NULL) {
 		const float *uv0, *uv1, *uv2;
 		double t = 1-u-v;
-		uv0 = &mesh->uv[2*i0];
-		uv1 = &mesh->uv[2*i1];
-		uv2 = &mesh->uv[2*i2];
+		uv0 = VEC2_NTH(mesh->uv, i0);
+		uv1 = VEC2_NTH(mesh->uv, i1);
+		uv2 = VEC2_NTH(mesh->uv, i2);
 		isect->uv[0] = t * uv0[0] + u * uv1[0] + v * uv2[0];
 		isect->uv[1] = t * uv0[1] + u * uv1[1] + v * uv2[1];
 	}
