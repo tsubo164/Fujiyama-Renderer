93e1400885551d499085927b7ee813918f6b10e8
Hiroshi Tsubokawa <hiroshi@fujiyama-renderer.com>
2013-03-22 23:04:03 -0700

    added release notes

---
+++ tests/generate_all_data.py
@@ -0,0 +1,57 @@
+#!/usr/bin/env python
+
+# generate all mesh, crv and mip data for example scenes
+# Copyright (c) 2011-2013 Hiroshi Tsubokawa
+
+import subprocess
+
+ply_list = [
+	'armadillo',
+	'bunny',
+	'dome',
+	'dragon',
+	'floor',
+	'grid',
+	'happy',
+	'horse',
+	'sphere',
+	'sphere2',
+	'teapot',
+	'xyzrgb_dragon']
+
+mesh_list = ['bunny']
+
+hdr_list = [
+	'doge2',
+	'ennis',
+	'forrest_salzburg02',
+	'glacier',
+	'grace-new',
+	'pisa',
+	'uffizi-large']
+
+try:
+	for name in ply_list:
+		src = '../../ply/'  + name + '.ply'
+		dst = '../../mesh/' + name + '.mesh'
+
+		print 'bin/ply2mesh', src, dst
+		return_code = subprocess.call(['bin/ply2mesh', src, dst])
+
+	for name in mesh_list:
+		src = '../../mesh/' + name + '.mesh'
+		dst = '../../crv/'  + name + '.crv'
+
+		print 'bin/curvegen', src, dst
+		return_code = subprocess.call(['bin/curvegen', src, dst])
+
+	for name in hdr_list:
+		src = '../../hdr/' + name + '.hdr'
+		dst = '../../mip/' + name + '.mip'
+
+		print 'bin/hdr2mip', src, dst
+		return_code = subprocess.call(['bin/hdr2mip', src, dst])
+
+except OSError, (errno, strerror):
+	print 'error: ' + strerror
+
---
+++ tests/run_all_scenes.py
@@ -0,0 +1,81 @@
+#!/usr/bin/env python
+
+# run all example scenes
+# Copyright (c) 2011-2013 Hiroshi Tsubokawa
+
+import subprocess
+
+scn_list = [
+	'scenes/furry_bunny.scn',
+	'scenes/happy_buddhas.scn',
+	'scenes/teapot.scn',
+	'scenes/xyzrgb_dragon.scn']
+
+py_list = [
+	'scenes/camera_motion_blur.py',
+	'scenes/dome_light1.py',
+	'scenes/dome_light2.py',
+	'scenes/glassy_happy.py',
+	'scenes/grid_light.py',
+	'scenes/pyro_ball.py',
+	'scenes/sphere_light.py',
+	'scenes/spline_wisps.py',
+	'scenes/subsurface_scattering.py',
+	'scenes/surface_wisps.py',
+	'scenes/transform_motion_blur.py',
+	'scenes/volume_and_bunny.py']
+
+try:
+	result = []
+
+	for filepath in scn_list:
+		lines = ""
+		for line in open(filepath):
+			line = line.replace('640 480', '160 120')
+			lines += line
+
+		print 'bin/scene', filepath
+		proc = subprocess.Popen(['bin/scene'], stdin=subprocess.PIPE,)
+		proc.communicate(lines)
+
+		if proc.returncode == -11:
+			result.append((filepath, "Segmentation fault"))
+		else:
+			result.append((filepath, "OK"))
+
+	for filepath in py_list:
+		lines = ""
+		for line in open(filepath):
+			line = line.replace('.Run(', '.Print(')
+			line = line.replace('640, 480', '160, 120')
+			lines += line
+
+		print 'python', filepath
+		proc = subprocess.Popen(['python'], stdin=subprocess.PIPE, stdout=subprocess.PIPE)
+		(stdoutdata, stderrdata) = proc.communicate(lines)
+
+		proc = subprocess.Popen(['bin/scene'], stdin=subprocess.PIPE,)
+		proc.communicate(stdoutdata)
+
+		if proc.returncode == -11:
+			result.append((filepath, "Segmentation fault"))
+		else:
+			result.append((filepath, "OK"))
+
+	total = len(result)
+	i = 1
+	for r in result:
+		print '%d/%d: %s: %s' % (i, total, r[0], r[1])
+		i += 1
+
+except KeyboardInterrupt:
+	print ''
+	print ''
+	print '===================='
+	print 'Rendering terminated'
+	print '===================='
+	print ''
+
+except OSError, (errno, strerror):
+	print 'error: ' + ': ' + strerror
+
