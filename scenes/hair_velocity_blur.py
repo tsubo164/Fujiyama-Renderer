3ee20ba61e0613e046613b1c44d985ce755b65e4
Hiroshi Tsubokawa <hiroshi@fujiyama-renderer.com>
2014-04-26 23:46:23 -0700

    added hair_velocity_blur.py

---
+++ scenes/hair_velocity_blur.py
@@ -0,0 +1,92 @@
+#!/usr/bin/env python
+
+# 1 hair curve and 1 head object with 1 dome light with an HDRI
+# Copyright (c) 2011-2014 Hiroshi Tsubokawa
+
+import fujiyama
+
+si = fujiyama.SceneInterface()
+
+#plugins
+si.OpenPlugin('ConstantShader')
+si.OpenPlugin('PlasticShader')
+si.OpenPlugin('HairShader')
+
+#Camera
+si.NewCamera('cam1', 'PerspectiveCamera')
+si.SetProperty3('cam1', 'translate', .08, -.05, 1.5)
+
+obj_rot = -90
+rot = 110
+
+#Light
+si.NewLight('light1', 'DomeLight')
+si.SetProperty3('light1', 'rotate', 0, rot, 0)
+#si.SetProperty1('light1', 'sample_count', 256)
+si.SetProperty1('light1', 'sample_count', 32)
+
+#Texture
+si.NewTexture('tex1', '../../mip/pisa.mip')
+si.AssignTexture('light1', 'environment_map', 'tex1');
+
+#Shader
+si.NewShader('head_shader', 'PlasticShader')
+si.SetProperty3('head_shader', 'diffuse', .8, .8, .8)
+
+si.NewShader('hair_shader', 'HairShader')
+
+si.NewShader('dome_shader', 'ConstantShader')
+si.AssignTexture('dome_shader', 'texture', 'tex1')
+
+#Mesh
+si.NewMesh('head_mesh', '../../mesh/head.mesh')
+si.NewMesh('dome_mesh', '../../mesh/dome.mesh')
+si.NewMesh('sphere_mesh', '../../mesh/sphere.mesh')
+
+#Curve
+si.NewCurve('hair_curve', '../../crv/hair.crv')
+
+#ObjectInstance
+si.NewObjectInstance('head1', 'head_mesh')
+si.SetProperty3('head1', 'rotate', 0, obj_rot, 0)
+si.AssignShader('head1', 'head_shader')
+
+si.NewObjectInstance('hair1', 'hair_curve')
+si.SetProperty3('hair1', 'rotate', 0, obj_rot, 0)
+si.AssignShader('hair1', 'hair_shader')
+
+si.NewObjectInstance('dome1', 'dome_mesh')
+si.SetProperty3('dome1', 'rotate', 0, rot, 0)
+si.SetProperty3('dome1', 'scale', -.5, .5, .5)
+si.AssignShader('dome1', 'dome_shader')
+
+#ObjectGroup
+# Create shadow_target for some objects.
+# Since 'DomeLight' has infinite distance, we need to exclude
+# 'dome1' object which is for just background image.
+si.NewObjectGroup('group1')
+si.AddObjectToGroup('group1', 'head1')
+si.AddObjectToGroup('group1', 'hair1')
+si.AssignObjectGroup('hair1', 'shadow_target', 'group1')
+si.AssignObjectGroup('head1', 'shadow_target', 'group1')
+
+#FrameBuffer
+si.NewFrameBuffer('fb1', 'rgba')
+
+#Renderer
+si.NewRenderer('ren1')
+si.AssignCamera('ren1', 'cam1')
+si.AssignFrameBuffer('ren1', 'fb1')
+si.SetProperty2('ren1', 'resolution', 640, 480)
+#si.SetProperty2('ren1', 'resolution', 160, 120)
+si.SetProperty2('ren1', 'pixelsamples', 9, 9)
+
+#Rendering
+si.RenderScene('ren1')
+
+#Output
+si.SaveFrameBuffer('fb1', '../hair_velocity_blur.fb')
+
+#Run commands
+si.Run()
+#si.Print()
