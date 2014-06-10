6f60f074b5ca1e9651bef965c64dbbdf04ff7ce8
Hiroshi Tsubokawa <hiroshi@fujiyama-renderer.com>
2014-06-10 14:42:03 -0700

    c++ ObjectInstance

--- src/fj_object_instance.cc
+++ src/fj_object_instance.cc
@@ -15,7 +15,6 @@ See LICENSE and README
 #include "fj_volume.h"
 #include "fj_matrix.h"
 #include "fj_array.h"
-#include "fj_box.h"
 #include "fj_ray.h"
 
 #include <assert.h>
@@ -24,212 +23,510 @@ See LICENSE and README
 
 namespace fj {
 
-struct ObjectInstance {
-  /* geometric properties */
-  const struct Accelerator *acc;
-  const struct Volume *volume;
-  struct Box bounds;
-
-  /* transformation properties */
-  struct TransformSampleList transform_samples;
-
-  /* non-geometric properties */
-  const struct Shader *shader;
-  const struct Light **target_lights;
-  int n_target_lights;
-  const struct ObjectGroup *reflection_target;
-  const struct ObjectGroup *refraction_target;
-  const struct ObjectGroup *shadow_target;
-  const struct ObjectGroup *self_target;
-};
-
 static void update_object_bounds(struct ObjectInstance *obj);
 static void merge_sampled_bounds(struct ObjectInstance *obj);
 
-/* ObjectInstance interfaces */
+ObjectInstance::ObjectInstance() :
+    acc_(NULL),
+    volume_(NULL),
+    bounds_(),
+
+    transform_samples_(),
+
+    shader_(NULL),
+    target_lights_(NULL),
+    n_target_lights_(0),
+
+    reflection_target_(NULL),
+    refraction_target_(NULL),
+    shadow_target_(NULL),
+    self_target_(NULL)
+{
+  XfmInitTransformSampleList(&transform_samples_);
+  update_bounds();
+}
+
+ObjectInstance::~ObjectInstance()
+{
+}
+
+int ObjectInstance::SetSurface(const Accelerator *acc)
+{
+  if (acc_ != NULL)
+    return -1;
+
+  if (volume_ != NULL)
+    return -1;
+
+  acc_ = acc;
+  update_bounds();
+
+  assert(acc_ != NULL && volume_ == NULL);
+  return 0;
+}
+
+int ObjectInstance::SetVolume(const Volume *volume)
+{
+  if (acc_ != NULL)
+    return -1;
+
+  if (volume_ != NULL)
+    return -1;
+
+  volume_ = volume;
+  update_bounds();
+
+  assert(acc_ == NULL && volume_ != NULL);
+  return 0;
+}
+
+bool ObjectInstance::IsSurface() const
+{
+  if (acc_ == NULL)
+    return false;
+
+  assert(volume_ == NULL);
+  return true;
+}
+
+bool ObjectInstance::IsVolume() const
+{
+  if (volume_ == NULL)
+    return false;
+
+  assert(acc_ == NULL);
+  return true;
+}
+
+void ObjectInstance::SetTranslate(double tx, double ty, double tz, double time)
+{
+  XfmPushTranslateSample(&transform_samples_, tx, ty, tz, time);
+  update_bounds();
+}
+
+void ObjectInstance::SetRotate(double rx, double ry, double rz, double time)
+{
+  XfmPushRotateSample(&transform_samples_, rx, ry, rz, time);
+  update_bounds();
+}
+
+void ObjectInstance::SetScale(double sx, double sy, double sz, double time)
+{
+  XfmPushScaleSample(&transform_samples_, sx, sy, sz, time);
+  update_bounds();
+}
+
+void ObjectInstance::SetTransformOrder(int order)
+{
+  XfmSetSampleTransformOrder(&transform_samples_, order);
+  update_bounds();
+}
+
+void ObjectInstance::SetRotateOrder(int order)
+{
+  XfmSetSampleRotateOrder(&transform_samples_, order);
+  update_bounds();
+}
+
+void ObjectInstance::SetShader(const Shader *shader)
+{
+  shader_ = shader;
+}
+
+void ObjectInstance::SetLightList(const Light **lights, int count)
+{
+  target_lights_ = lights;
+  n_target_lights_ = count;
+}
+
+void ObjectInstance::SetReflectTarget(const ObjectGroup *group)
+{
+  reflection_target_ = group;
+}
+
+void ObjectInstance::SetRefractTarget(const ObjectGroup *group)
+{
+  refraction_target_ = group;
+}
+
+void ObjectInstance::SetShadowTarget(const ObjectGroup *group)
+{
+  shadow_target_ = group;
+}
+
+// TODO come up with better way to make self target
+void ObjectInstance::SetSelfHitTarget(const ObjectGroup *group)
+{
+  self_target_ = group;
+}
+
+const ObjectGroup *ObjectInstance::GetReflectTarget() const
+{
+  return reflection_target_;
+}
+
+const ObjectGroup *ObjectInstance::GetRefractTarget() const
+{
+  return refraction_target_;
+}
+
+const ObjectGroup *ObjectInstance::GetShadowTarget() const
+{
+  return shadow_target_;
+}
+
+const ObjectGroup *ObjectInstance::GetSelfHitTarget() const
+{
+  return self_target_;
+}
+
+const Shader *ObjectInstance::GetShader() const
+{
+  return shader_;
+}
+
+const Light **ObjectInstance::GetLightList() const
+{
+  return target_lights_;
+}
+
+int ObjectInstance::GetLightCount() const
+{
+  return n_target_lights_;
+}
+
+const Box &ObjectInstance::GetBounds() const
+{
+  return bounds_;
+}
+
+void ObjectInstance::ComputeBounds()
+{
+  update_bounds();
+}
+
+bool ObjectInstance::Intersect(const Ray &ray, double time, Intersection *isect) const
+{
+  Transform transform_interp;
+  Ray ray_object_space;
+  int hit = 0;
+
+  if (!IsSurface())
+    return 0;
+
+  XfmLerpTransformSample(&transform_samples_, time, &transform_interp);
+
+  // transform ray to object space
+  ray_object_space = ray;
+  XfmTransformPointInverse(&transform_interp, &ray_object_space.orig);
+  XfmTransformVectorInverse(&transform_interp, &ray_object_space.dir);
+
+  hit = acc_->Intersect(ray_object_space, time, isect);
+  if (!hit)
+    return 0;
+
+  // transform intersection back to world space
+  XfmTransformPoint(&transform_interp, &isect->P);
+  XfmTransformVector(&transform_interp, &isect->N);
+  Normalize(&isect->N);
+
+  XfmTransformVector(&transform_interp, &isect->dPdu);
+  XfmTransformVector(&transform_interp, &isect->dPdv);
+
+  isect->object = this;
+
+  return 1;
+}
+
+bool ObjectInstance::VolumeIntersect(const Ray &ray, double time, Interval *interval) const
+{
+  struct Transform transform_interp;
+  struct Ray ray_object_space;
+  struct Box volume_bounds;
+  double boxhit_tmin = 0;
+  double boxhit_tmax = 0;
+  int hit = 0;
+
+  if (!IsVolume())
+    return 0;
+
+  volume_bounds = volume_->GetBounds();
+
+  XfmLerpTransformSample(&transform_samples_, time, &transform_interp);
+
+  /* transform ray to object space */
+  ray_object_space = ray;
+  XfmTransformPointInverse(&transform_interp, &ray_object_space.orig);
+  XfmTransformVectorInverse(&transform_interp, &ray_object_space.dir);
+
+  hit = BoxRayIntersect(volume_bounds,
+      ray_object_space.orig,
+      ray_object_space.dir,
+      ray_object_space.tmin,
+      ray_object_space.tmax,
+      &boxhit_tmin, &boxhit_tmax);
+
+  if (!hit) {
+    return 0;
+  }
+
+  interval->tmin = boxhit_tmin;
+  interval->tmax = boxhit_tmax;
+  interval->object = this;
+
+  return 1;
+}
+
+bool ObjectInstance::GetVolumeSample(const Vector &point, double time,
+    VolumeSample *sample) const
+{
+  struct Transform transform_interp;
+  struct Vector point_in_objspace;
+  int hit = 0;
+
+  if (!IsVolume())
+    return 0;
+
+  XfmLerpTransformSample(&transform_samples_, time, &transform_interp);
+
+  point_in_objspace = point;
+  XfmTransformPointInverse(&transform_interp, &point_in_objspace);
+
+  hit = volume_->GetSample(point_in_objspace, sample);
+
+  return hit;
+}
+
+void ObjectInstance::update_bounds()
+{
+  if (IsSurface()) {
+    bounds_ = acc_->GetBounds();
+  }
+  else if (IsVolume()) {
+    bounds_ = volume_->GetBounds();
+  }
+  else {
+    bounds_ = Box();
+  }
+  merge_sampled_bounds();
+}
+
+void ObjectInstance::merge_sampled_bounds()
+{
+  Box merged_bounds(FLT_MAX, FLT_MAX, FLT_MAX, -FLT_MAX, -FLT_MAX, -FLT_MAX);
+  Box original_bounds(FLT_MAX, FLT_MAX, FLT_MAX, -FLT_MAX, -FLT_MAX, -FLT_MAX);
+  double S[3] = {1, 1, 1};
+  int i;
+
+  // assumes bounds_ is the same as acc->bounds or vol->bounds
+  original_bounds = GetBounds();
+
+  // extend bounds when rotated to ensure object is always inside bounds
+  if (transform_samples_.rotate.sample_count > 1) {
+    const double diagonal = BoxDiagonal(original_bounds);
+    const Vector centroid = BoxCentroid(original_bounds);
+
+    original_bounds = Box(
+        centroid.x - diagonal,
+        centroid.y - diagonal,
+        centroid.z - diagonal,
+        centroid.x + diagonal,
+        centroid.y + diagonal,
+        centroid.z + diagonal);
+  }
+
+  // compute maximum scale over sampling time
+  S[0] = transform_samples_.scale.samples[0].vector[0];
+  S[1] = transform_samples_.scale.samples[0].vector[1];
+  S[2] = transform_samples_.scale.samples[0].vector[2];
+  for (i = 1; i < transform_samples_.scale.sample_count; i++) {
+    S[0] = Max(S[0], transform_samples_.scale.samples[i].vector[0]);
+    S[1] = Max(S[1], transform_samples_.scale.samples[i].vector[1]);
+    S[2] = Max(S[2], transform_samples_.scale.samples[i].vector[2]);
+  }
+
+  // accumulate all bounds over sampling time
+  for (i = 0; i < transform_samples_.translate.sample_count; i++) {
+    Box sample_bounds(FLT_MAX, FLT_MAX, FLT_MAX, -FLT_MAX, -FLT_MAX, -FLT_MAX);
+    const double *T = transform_samples_.translate.samples[i].vector;
+    const double *R = transform_samples_.rotate.samples[i].vector;
+    Transform transform;
+
+    XfmSetTransform(&transform,
+      transform_samples_.transform_order, transform_samples_.rotate_order,
+      T[0], T[1], T[2],
+      R[0], R[1], R[2],
+      S[0], S[1], S[2]);
+
+    sample_bounds = original_bounds;
+    XfmTransformBounds(&transform, &sample_bounds);
+    BoxAddBox(&merged_bounds, sample_bounds);
+  }
+
+  bounds_ = merged_bounds;
+}
+
 struct ObjectInstance *ObjNew(void)
 {
-  struct ObjectInstance *obj = FJ_MEM_ALLOC(struct ObjectInstance);
-
-  if (obj == NULL)
-    return NULL;
-
-  obj->acc = NULL;
-  obj->volume = NULL;
-
-  XfmInitTransformSampleList(&obj->transform_samples);
-  update_object_bounds(obj);
-
-  obj->shader = NULL;
-  obj->target_lights = NULL;
-  obj->n_target_lights = 0;
-
-  obj->reflection_target = NULL;
-  obj->refraction_target = NULL;
-  obj->shadow_target = NULL;
-  obj->self_target = NULL;
-
-  return obj;
+  return new ObjectInstance();
 }
 
 void ObjFree(struct ObjectInstance *obj)
 {
-  if (obj == NULL)
-    return;
-  FJ_MEM_FREE(obj);
+  delete obj;
 }
 
 int ObjSetSurface(struct ObjectInstance *obj, const struct Accelerator *acc)
 {
-  if (obj->acc != NULL)
+  if (obj->acc_ != NULL)
     return -1;
 
-  if (obj->volume != NULL)
+  if (obj->volume_ != NULL)
     return -1;
 
-  obj->acc = acc;
+  obj->acc_ = acc;
   update_object_bounds(obj);
 
-  assert(obj->acc != NULL && obj->volume == NULL);
+  assert(obj->acc_ != NULL && obj->volume_ == NULL);
   return 0;
 }
 
 int ObjSetVolume(struct ObjectInstance *obj, const struct Volume *volume)
 {
-  if (obj->acc != NULL)
+  if (obj->acc_ != NULL)
     return -1;
 
-  if (obj->volume != NULL)
+  if (obj->volume_ != NULL)
     return -1;
 
-  obj->volume = volume;
+  obj->volume_ = volume;
   update_object_bounds(obj);
 
-  assert(obj->acc == NULL && obj->volume != NULL);
+  assert(obj->acc_ == NULL && obj->volume_ != NULL);
   return 0;
 }
 
 int ObjIsSurface(const struct ObjectInstance *obj)
 {
-  if (obj->acc == NULL)
+  if (obj->acc_ == NULL)
     return 0;
 
-  assert(obj->volume == NULL);
+  assert(obj->volume_ == NULL);
   return 1;
 }
 
 int ObjIsVolume(const struct ObjectInstance *obj)
 {
-  if (obj->volume == NULL)
+  if (obj->volume_ == NULL)
     return 0;
 
-  assert(obj->acc == NULL);
+  assert(obj->acc_ == NULL);
   return 1;
 }
 
 void ObjSetTranslate(struct ObjectInstance *obj,
     double tx, double ty, double tz, double time)
 {
-  XfmPushTranslateSample(&obj->transform_samples, tx, ty, tz, time);
+  XfmPushTranslateSample(&obj->transform_samples_, tx, ty, tz, time);
   update_object_bounds(obj);
 }
 
 void ObjSetRotate(struct ObjectInstance *obj,
     double rx, double ry, double rz, double time)
 {
-  XfmPushRotateSample(&obj->transform_samples, rx, ry, rz, time);
+  XfmPushRotateSample(&obj->transform_samples_, rx, ry, rz, time);
   update_object_bounds(obj);
 }
 
 void ObjSetScale(struct ObjectInstance *obj,
     double sx, double sy, double sz, double time)
 {
-  XfmPushScaleSample(&obj->transform_samples, sx, sy, sz, time);
+  XfmPushScaleSample(&obj->transform_samples_, sx, sy, sz, time);
   update_object_bounds(obj);
 }
 
 void ObjSetTransformOrder(struct ObjectInstance *obj, int order)
 {
-  XfmSetSampleTransformOrder(&obj->transform_samples, order);
+  XfmSetSampleTransformOrder(&obj->transform_samples_, order);
   update_object_bounds(obj);
 }
 
 void ObjSetRotateOrder(struct ObjectInstance *obj, int order)
 {
-  XfmSetSampleRotateOrder(&obj->transform_samples, order);
+  XfmSetSampleRotateOrder(&obj->transform_samples_, order);
   update_object_bounds(obj);
 }
 
 void ObjSetShader(struct ObjectInstance *obj, const struct Shader *shader)
 {
-  obj->shader = shader;
+  obj->shader_ = shader;
 }
 
 void ObjSetLightList(struct ObjectInstance *obj, const struct Light **lights, int count)
 {
-  obj->target_lights = lights;
-  obj->n_target_lights = count;
+  obj->target_lights_ = lights;
+  obj->n_target_lights_ = count;
 }
 
 void ObjSetReflectTarget(struct ObjectInstance *obj, const struct ObjectGroup *grp)
 {
-  obj->reflection_target = grp;
+  obj->reflection_target_ = grp;
 }
 
 void ObjSetRefractTarget(struct ObjectInstance *obj, const struct ObjectGroup *grp)
 {
-  obj->refraction_target = grp;
+  obj->refraction_target_ = grp;
 }
 
 void ObjSetShadowTarget(struct ObjectInstance *obj, const struct ObjectGroup *grp)
 {
-  obj->shadow_target = grp;
+  obj->shadow_target_ = grp;
 }
 
 /* TODO come up with better way to make self target */
 void ObjSetSelfHitTarget(struct ObjectInstance *obj, const struct ObjectGroup *grp)
 {
-  obj->self_target = grp;
+  obj->self_target_ = grp;
 }
 
 const struct ObjectGroup *ObjGetReflectTarget(const struct ObjectInstance *obj)
 {
-  return obj->reflection_target;
+  return obj->reflection_target_;
 }
 
 const struct ObjectGroup *ObjGetRefractTarget(const struct ObjectInstance *obj)
 {
-  return obj->refraction_target;
+  return obj->refraction_target_;
 }
 
 const struct ObjectGroup *ObjGetShadowTarget(const struct ObjectInstance *obj)
 {
-  return obj->shadow_target;
+  return obj->shadow_target_;
 }
 
 const struct ObjectGroup *ObjGetSelfHitTarget(const struct ObjectInstance *obj)
 {
-  return obj->self_target;
+  return obj->self_target_;
 }
 
 const struct Shader *ObjGetShader(const struct ObjectInstance *obj)
 {
-  return obj->shader;
+  return obj->shader_;
 }
 
 const struct Light **ObjGetLightList(const struct ObjectInstance *obj)
 {
-  return obj->target_lights;
+  return obj->target_lights_;
 }
 
 int ObjGetLightCount(const struct ObjectInstance *obj)
 {
-  return obj->n_target_lights;
+  return obj->n_target_lights_;
 }
 
 void ObjGetBounds(const struct ObjectInstance *obj, struct Box *bounds)
 {
-  *bounds = obj->bounds;
+  *bounds = obj->bounds_;
 }
 
 void ObjComputeBounds(struct ObjectInstance *obj)
@@ -247,14 +544,14 @@ int ObjIntersect(const struct ObjectInstance *obj, double time,
   if (!ObjIsSurface(obj))
     return 0;
 
-  XfmLerpTransformSample(&obj->transform_samples, time, &transform_interp);
+  XfmLerpTransformSample(&obj->transform_samples_, time, &transform_interp);
 
   /* transform ray to object space */
   ray_object_space = *ray;
   XfmTransformPointInverse(&transform_interp, &ray_object_space.orig);
   XfmTransformVectorInverse(&transform_interp, &ray_object_space.dir);
 
-  hit = obj->acc->Intersect(ray_object_space, time, isect);
+  hit = obj->acc_->Intersect(ray_object_space, time, isect);
   if (!hit)
     return 0;
 
@@ -284,9 +581,9 @@ int ObjVolumeIntersect(const struct ObjectInstance *obj, double time,
   if (!ObjIsVolume(obj))
     return 0;
 
-  volume_bounds = obj->volume->GetBounds();
+  volume_bounds = obj->volume_->GetBounds();
 
-  XfmLerpTransformSample(&obj->transform_samples, time, &transform_interp);
+  XfmLerpTransformSample(&obj->transform_samples_, time, &transform_interp);
 
   /* transform ray to object space */
   ray_object_space = *ray;
@@ -321,12 +618,12 @@ int ObjGetVolumeSample(const struct ObjectInstance *obj, double time,
   if (!ObjIsVolume(obj))
     return 0;
 
-  XfmLerpTransformSample(&obj->transform_samples, time, &transform_interp);
+  XfmLerpTransformSample(&obj->transform_samples_, time, &transform_interp);
 
   point_in_objspace = *point;
   XfmTransformPointInverse(&transform_interp, &point_in_objspace);
 
-  hit = obj->volume->GetSample(point_in_objspace, sample);
+  hit = obj->volume_->GetSample(point_in_objspace, sample);
 
   return hit;
 }
@@ -334,13 +631,13 @@ int ObjGetVolumeSample(const struct ObjectInstance *obj, double time,
 static void update_object_bounds(struct ObjectInstance *obj)
 {
   if (ObjIsSurface(obj)) {
-    obj->bounds = obj->acc->GetBounds();
+    obj->bounds_ = obj->acc_->GetBounds();
   }
   else if (ObjIsVolume(obj)) {
-    obj->bounds = obj->volume->GetBounds();
+    obj->bounds_ = obj->volume_->GetBounds();
   }
   else {
-    obj->bounds = Box();
+    obj->bounds_ = Box();
   }
   merge_sampled_bounds(obj);
 }
@@ -352,11 +649,11 @@ static void merge_sampled_bounds(struct ObjectInstance *obj)
   double S[3] = {1, 1, 1};
   int i;
 
-  /* assumes obj->bounds is the same as acc->bounds or vol->bounds */
+  /* assumes obj->bounds_ is the same as acc->bounds or vol->bounds */
   ObjGetBounds(obj, &original_bounds);
 
   /* extend bounds when rotated to ensure object is always inside bounds */
-  if (obj->transform_samples.rotate.sample_count > 1) {
+  if (obj->transform_samples_.rotate.sample_count > 1) {
     const double diagonal = BoxDiagonal(original_bounds);
     const Vector centroid = BoxCentroid(original_bounds);
 
@@ -370,24 +667,24 @@ static void merge_sampled_bounds(struct ObjectInstance *obj)
   }
 
   /* compute maximum scale over sampling time */
-  S[0] = obj->transform_samples.scale.samples[0].vector[0];
-  S[1] = obj->transform_samples.scale.samples[0].vector[1];
-  S[2] = obj->transform_samples.scale.samples[0].vector[2];
-  for (i = 1; i < obj->transform_samples.scale.sample_count; i++) {
-    S[0] = Max(S[0], obj->transform_samples.scale.samples[i].vector[0]);
-    S[1] = Max(S[1], obj->transform_samples.scale.samples[i].vector[1]);
-    S[2] = Max(S[2], obj->transform_samples.scale.samples[i].vector[2]);
+  S[0] = obj->transform_samples_.scale.samples[0].vector[0];
+  S[1] = obj->transform_samples_.scale.samples[0].vector[1];
+  S[2] = obj->transform_samples_.scale.samples[0].vector[2];
+  for (i = 1; i < obj->transform_samples_.scale.sample_count; i++) {
+    S[0] = Max(S[0], obj->transform_samples_.scale.samples[i].vector[0]);
+    S[1] = Max(S[1], obj->transform_samples_.scale.samples[i].vector[1]);
+    S[2] = Max(S[2], obj->transform_samples_.scale.samples[i].vector[2]);
   }
 
   /* accumulate all bounds over sampling time */
-  for (i = 0; i < obj->transform_samples.translate.sample_count; i++) {
+  for (i = 0; i < obj->transform_samples_.translate.sample_count; i++) {
     struct Box sample_bounds(FLT_MAX, FLT_MAX, FLT_MAX, -FLT_MAX, -FLT_MAX, -FLT_MAX);
-    const double *T = obj->transform_samples.translate.samples[i].vector;
-    const double *R = obj->transform_samples.rotate.samples[i].vector;
+    const double *T = obj->transform_samples_.translate.samples[i].vector;
+    const double *R = obj->transform_samples_.rotate.samples[i].vector;
     struct Transform transform;
 
     XfmSetTransform(&transform,
-      obj->transform_samples.transform_order, obj->transform_samples.rotate_order,
+      obj->transform_samples_.transform_order, obj->transform_samples_.rotate_order,
       T[0], T[1], T[2],
       R[0], R[1], R[2],
       S[0], S[1], S[2]);
@@ -397,7 +694,7 @@ static void merge_sampled_bounds(struct ObjectInstance *obj)
     BoxAddBox(&merged_bounds, sample_bounds);
   }
 
-  obj->bounds = merged_bounds;
+  obj->bounds_ = merged_bounds;
 }
 
 } // namespace xxx
--- src/fj_object_instance.h
+++ src/fj_object_instance.h
@@ -6,8 +6,8 @@ See LICENSE and README
 #ifndef FJ_OBJECTINSTANCE_H
 #define FJ_OBJECTINSTANCE_H
 
-/* to make transform_order visible for fj_object_instance.h's users */
 #include "fj_transform.h"
+#include "fj_box.h"
 
 namespace fj {
 
@@ -21,13 +21,69 @@ struct Light;
 struct Box;
 struct Ray;
 
-struct ObjectInstance;
-struct ObjectGroup;
+class ObjectGroup;
 
-/* TODO not used anymore? */
-struct ObjectContent {
-  const struct Accelerator *surface;
-  const struct Volume *volume;
+class ObjectInstance {
+public:
+  ObjectInstance();
+  ~ObjectInstance();
+
+  int SetSurface(const Accelerator *acc);
+  int SetVolume(const Volume *volume);
+  bool IsSurface() const;
+  bool IsVolume() const;
+
+  // transformation
+  void SetTranslate(double tx, double ty, double tz, double time);
+  void SetRotate(double rx, double ry, double rz, double time);
+  void SetScale(double sx, double sy, double sz, double time);
+  void SetTransformOrder(int order);
+  void SetRotateOrder(int order);
+
+  // non-geometric properties */
+  void SetShader(const Shader *shader);
+  void SetLightList(const Light **lights, int count);
+  void SetReflectTarget(const ObjectGroup *group);
+  void SetRefractTarget(const ObjectGroup *group);
+  void SetShadowTarget(const ObjectGroup *group);
+  void SetSelfHitTarget(const ObjectGroup *group);
+
+  const ObjectGroup *GetReflectTarget() const;
+  const ObjectGroup *GetRefractTarget() const;
+  const ObjectGroup *GetShadowTarget() const;
+  const ObjectGroup *GetSelfHitTarget() const;
+
+  const Shader *GetShader() const;
+  const Light **GetLightList() const;
+  int GetLightCount() const;
+  const Box &GetBounds() const;
+  void ComputeBounds();
+
+  // sampling
+  bool Intersect(const Ray &ray, double time, Intersection *isect) const;
+  bool VolumeIntersect(const Ray &ray, double time, Interval *interval) const;
+  bool GetVolumeSample(const Vector &point, double time, VolumeSample *sample) const;
+
+public:
+  void update_bounds();
+  void merge_sampled_bounds();
+
+  // geometric properties
+  const Accelerator *acc_;
+  const Volume *volume_;
+  Box bounds_;
+
+  // transformation properties
+  TransformSampleList transform_samples_;
+
+  // non-geometric properties
+  const Shader *shader_;
+  const Light **target_lights_;
+  int n_target_lights_;
+  const ObjectGroup *reflection_target_;
+  const ObjectGroup *refraction_target_;
+  const ObjectGroup *shadow_target_;
+  const ObjectGroup *self_target_;
 };
 
 /* ObjectInstance interfaces */
@@ -78,4 +134,4 @@ extern int ObjGetVolumeSample(const struct ObjectInstance *obj, double time,
 
 } // namespace xxx
 
-#endif /* FJ_XXX_H */
+#endif // FJ_XXX_H
