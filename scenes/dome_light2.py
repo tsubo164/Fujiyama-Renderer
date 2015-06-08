b2f0dbc8fcdaec865fb7908b106c7b615ceac33c
Hiroshi Tsubokawa <hiroshi@fujiyama-renderer.com>
2015-06-07 23:39:26 -0700

    DEFAULT_SHADING_GROUP to scenes/dome_light2.py

--- scenes/dome_light2.py
+++ scenes/dome_light2.py
@@ -23,7 +23,7 @@ si.SetProperty3('light1', 'rotate', 0, rot, 0)
 si.SetProperty1('light1', 'sample_count', 256)
 
 #Texture
-si.NewTexture('tex1', '../../mip/forrest_salzburg02.mip')
+si.NewTexture('tex1', '../../hdr/forrest_salzburg02.hdr')
 si.AssignTexture('light1', 'environment_map', 'tex1');
 
 #Shader
@@ -42,26 +42,26 @@ si.SetProperty3('sphere_shader2', 'diffuse', .5, .5, .5)
 si.SetProperty3('sphere_shader2', 'reflect', 0, 0, 0)
 
 #Mesh
-si.NewMesh('happy_mesh', '../../mesh/happy.mesh')
-si.NewMesh('dome_mesh', '../../mesh/dome.mesh')
-si.NewMesh('sphere_mesh', '../../mesh/sphere.mesh')
+si.NewMesh('happy_mesh', '../../ply/happy.ply')
+si.NewMesh('dome_mesh', '../../ply/dome.ply')
+si.NewMesh('sphere_mesh', '../../ply/sphere.ply')
 
 #ObjectInstance
 si.NewObjectInstance('happy1', 'happy_mesh')
-si.AssignShader('happy1', 'happy_shader')
+si.AssignShader('happy1', 'DEFAULT_SHADING_GROUP', 'happy_shader')
 
 si.NewObjectInstance('dome1', 'dome_mesh')
 si.SetProperty3('dome1', 'rotate', 0, rot, 0)
 si.SetProperty3('dome1', 'scale', -.5, .5, .5)
-si.AssignShader('dome1', 'dome_shader')
+si.AssignShader('dome1', 'DEFAULT_SHADING_GROUP', 'dome_shader')
 
 si.NewObjectInstance('sphere1', 'sphere_mesh')
-si.AssignShader('sphere1', 'sphere_shader1')
+si.AssignShader('sphere1', 'DEFAULT_SHADING_GROUP', 'sphere_shader1')
 si.SetProperty3('sphere1', 'translate', -1.5, -.5, 0)
 si.SetProperty3('sphere1', 'scale', .5, .5, .5)
 
 si.NewObjectInstance('sphere2', 'sphere_mesh')
-si.AssignShader('sphere2', 'sphere_shader2')
+si.AssignShader('sphere2', 'DEFAULT_SHADING_GROUP', 'sphere_shader2')
 si.SetProperty3('sphere2', 'translate', 1.5, -.5, 0)
 si.SetProperty3('sphere2', 'scale', .5, .5, .5)
 
@@ -92,7 +92,6 @@ si.AssignCamera('ren1', 'cam1')
 si.AssignFrameBuffer('ren1', 'fb1')
 si.SetProperty2('ren1', 'resolution', 640, 480)
 #si.SetProperty2('ren1', 'resolution', 160, 120)
-#si.SetProperty2('ren1', 'pixelsamples', 9, 9)
 
 #Rendering
 si.RenderScene('ren1')
